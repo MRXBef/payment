3363b4576724bc9331732103166b31f4d6cd92d8
MRXbef <adambayu878@gmail.com>
2025-10-15 17:53:45 +0800

    repair view

--- payment/src/App.jsx
+++ payment/src/App.jsx
@@ -93,10 +93,11 @@ function App() {
         <h1 style={{ fontSize: "1rem" }}>IDR 167,000,00</h1>
         <div style={{paddingLeft: "15px", paddingRight: "15px"}}>
           <p style={{ fontSize: "0.6rem" }} className="read-the-docs">
-            Scan the QR Code and Paste the "Transaction_ID" or "Reference_ID"  into the Variable at the configuration code. Do it before {monthList[month]}{" "}
+            Scan the QR Code and Paste the "Transaction_ID" or "Reference_ID"  into the Variable at the configuration code and make sure the payment amount is correct. Do it before 18 {monthList[month]}{" "}.
             {new Date().getFullYear()}
             <tr></tr>
-            Ignore this if you have already made a payment.
+            <tr></tr>
+            Ignore this Instruction if you have already made a payment this month.
           </p>
         </div>
       </div>
